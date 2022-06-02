522c6005a4780b93e906bb2e229c8e0c5ca7c8d0
yxxyn20 <dbtndus0527@gmail.com>
2022-06-02 17:57:41 +0900

    #4 메인페이지 UI 제작

--- TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/MainActivity.kt
+++ TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.travelcultureapplicaiton
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.travelcultureapplicaiton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { // 옵션 메뉴 추가
+        //리소스로 만든 메뉴 적용
+        menuInflater.inflate(R.menu.menu_main_setting, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // 메뉴의 아이템이 선택되었을 때 이벤트 처리
+        when(item.itemId){
+            R.id.settings -> {
+                // 설정 화면으로 이동
+                val intent = Intent(this, SettingActivity::class.java)
+                startActivity(intent)
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
