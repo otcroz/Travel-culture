f314eea752ef36cb6c973a9392924629414ff06e
yxxyn20 <dbtndus0527@gmail.com>
2022-06-02 15:28:26 +0900

    #3 로그인, 회원가입 UI 구성
    - 로그인 UI
    - 회원가입 UI
    - 로그인 페이지에서 "회원가입하기" 눌렀을 때 회원가입으로 넘어가는 기능
    - 회원가입을 마치고 로그인 페이지로 넘어가는 기능

--- TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/LoginActivity.kt
+++ TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/LoginActivity.kt
@@ -1,11 +1,30 @@
 package com.example.travelcultureapplicaiton
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.travelcultureapplicaiton.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        var binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 회원가입 페이지 이동
+        binding.gotoSignUp.setOnClickListener {
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 이메일 로그인
+
+
+        // 카카오 간편 로그인
+
+
+        // 페이스북 간편 로그인
     }
 }
--- TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/RegisterActivity.kt
+++ TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/RegisterActivity.kt
@@ -1,11 +1,83 @@
 package com.example.travelcultureapplicaiton
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import com.example.travelcultureapplicaiton.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        var binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nextButton = binding.nextSignIn
+        val nextSuccess = binding.nextSuccess
+        val inputName = binding.inputName
+        val residence = binding.residence
+        val favoriteCategory = binding.favoriteCategory
+        val newEmail = binding.newEmail
+        val inputNewpassword = binding.inputNewpassword
+        val checkPassword = binding.checkPassword
+
+        //모든 EditText 값 > 0 일때 다음 화면으로 넘어가도록 한다.
+        var textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (newEmail.length() > 0 && inputNewpassword.length() > 0 && checkPassword.length() > 0){
+                    nextButton.isEnabled = true
+                    nextButton.setBackgroundColor(Color.parseColor("#B4D480"));
+                    if(residence.length() > 0 && favoriteCategory.length() > 0 && inputName.length() > 0){
+                        nextSuccess.isEnabled = true
+                        nextSuccess.setBackgroundColor(Color.parseColor("#B4D480"));
+                    }
+                } else{
+                    nextButton.isEnabled = false
+                    nextButton.setBackgroundColor(Color.parseColor("#d3d3d3"));
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        }
+
+        // 이벤트 핸들러 연결하기
+        newEmail.addTextChangedListener(textWatcher)
+        inputNewpassword.addTextChangedListener(textWatcher)
+        checkPassword.addTextChangedListener(textWatcher)
+        residence.addTextChangedListener(textWatcher)
+        favoriteCategory.addTextChangedListener(textWatcher)
+        inputName.addTextChangedListener(textWatcher)
+
+        // 다음으로 버튼 클릭
+        nextButton.setOnClickListener {
+            nextButton.setBackgroundColor(Color.parseColor("#d3d3d3"))
+            nextButton.text = "입력완료~!"
+            // 버튼 색깔 설정하기
+            inputName.visibility = View.VISIBLE
+            residence.visibility = View.VISIBLE
+            favoriteCategory.visibility = View.VISIBLE
+            nextSuccess.visibility = View.VISIBLE
+
+            // 이전에 입력했던 창들은 보이지 않게
+            newEmail.visibility = View.GONE
+            inputNewpassword.visibility = View.GONE
+            checkPassword.visibility = View.GONE
+            nextButton.visibility = View.GONE
+
+
+        }
+        nextSuccess.setOnClickListener {
+            // db에 회원 정보 옮기는 작업 수행
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
