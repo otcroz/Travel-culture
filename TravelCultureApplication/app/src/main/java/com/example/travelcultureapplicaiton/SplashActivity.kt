5f29bce855e6d449654874e206ebdd8855d432ef
yxxyn20 <dbtndus0527@gmail.com>
2022-06-04 04:24:51 +0900

    #2 스플래시 화면 제작

--- TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/SplashActivity.kt
+++ TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.travelcultureapplicaiton
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        loadSplashScreen()
+    }
+    private fun loadSplashScreen(){
+        Handler().postDelayed({
+            // You can declare your desire activity here to open after finishing splash screen. Like MainActivity
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
