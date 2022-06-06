f5900f2d7afbb1fbed0902b1142b72117d2c7d96
yxxyn20 <dbtndus0527@gmail.com>
2022-06-07 03:21:27 +0900

    #6 프래그먼트에서 Google Map 띄우기, 사용자 위치 정보
    - 마커 띄우기
    - 사용자의 위치 정보 퍼미션 -> 지도에 사용자의 위치 기준으로 지도가 보이도록 하기

--- TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/MapFragment.kt
+++ TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/MapFragment.kt
@@ -1,10 +1,28 @@
 package com.example.travelcultureapplicaiton
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnSuccessListener
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +34,18 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MapFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var mView: MapView
+    var googleMap: GoogleMap? = null
+
+    // 사용자 위치정보에 대한 클라이언트 변수
+    lateinit var apiClient : GoogleApiClient
+    lateinit var providerClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +59,101 @@ class MapFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        var rootView = inflater.inflate(R.layout.fragment_map, container, false)
+        mView = rootView.findViewById(R.id.mapView)
+        mView.onCreate(savedInstanceState)
+        mView.getMapAsync(this)
+
+        // 사용자 퍼미션 얻기
+        providerClient = LocationServices.getFusedLocationProviderClient(activity as MainActivity)
+        apiClient = GoogleApiClient.Builder(activity as MainActivity)
+            .addApi(LocationServices.API)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .build()
+
+        val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            if(it.all{permission -> permission.value == true}){
+                apiClient.connect() // 정보 가져옴, onConnected와 연결
+            } else{
+                Toast.makeText(activity as MainActivity,"권한 거부..", Toast.LENGTH_SHORT).show()
+            }
+        }
+        if(ContextCompat.checkSelfPermission(activity as MainActivity, Manifest.permission.ACCESS_FINE_LOCATION) !== PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(activity as MainActivity,Manifest.permission.WRITE_EXTERNAL_STORAGE) !== PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(activity as MainActivity,Manifest.permission.ACCESS_NETWORK_STATE) !== PackageManager.PERMISSION_GRANTED)
+        { Log.d("mobileApp", "checkSelfPermission")
+            requestPermissionLauncher.launch(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_NETWORK_STATE)
+            )
+        }
+        else{
+            apiClient.connect() // 정보 가져옴
+        }
+
+        return rootView
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        googleMap = p0 // 구글맵을 변수에 넣는다.
+        moveMap(37.568256, 126.897240)
+        /*val latLng = LatLng(37.568256, 126.897240)
+        val position: CameraPosition = CameraPosition.Builder() // 카메라를 움직여서 설정해놓은 위치로 지도를 이동하도록 한다.
+            .target(latLng)
+            .zoom(16f)
+            .build()
+        googleMap!!.moveCamera(CameraUpdateFactory.newCameraPosition(position))
+
+        // 마커 추가하기
+        val markerOp = MarkerOptions()
+        markerOp.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        markerOp.position(latLng)
+        markerOp.title("월드컵경기장")
+        googleMap?.addMarker(markerOp)*/
+    }
+
+    // 카메라를 이동시키는 함수
+    private fun moveMap(latitude: Double, longitude: Double){
+        val latLng = LatLng(latitude, longitude)
+        val position: CameraPosition = CameraPosition.Builder() // 카메라를 움직여서 설정해놓은 위치로 지도를 이동하도록 한다.
+            .target(latLng)
+            .zoom(16f)
+            .build()
+        googleMap!!.moveCamera(CameraUpdateFactory.newCameraPosition(position))
+
+        // 마커 추가하기
+        val markerOp = MarkerOptions()
+        markerOp.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        markerOp.position(latLng)
+        markerOp.title("MyLocation")
+        googleMap?.addMarker(markerOp)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mView.onStart()
+    }
+    override fun onStop() {
+        super.onStop()
+        mView.onStop()
+    }
+    override fun onResume() {
+        super.onResume()
+        mView.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mView.onPause()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mView.onLowMemory()
+    }
+    override fun onDestroy() {
+        mView.onDestroy()
+        super.onDestroy()
     }
 
     companion object {
@@ -56,4 +175,36 @@ class MapFragment : Fragment() {
                 }
             }
     }
+
+    override fun onConnected(p0: Bundle?) {
+        // 퍼미션 다시 확인
+        Log.d("mobileApp", "onConnected")
+        if(ContextCompat.checkSelfPermission(activity as MainActivity, Manifest.permission.ACCESS_FINE_LOCATION) === PackageManager.PERMISSION_GRANTED){
+            providerClient.lastLocation.addOnSuccessListener(
+                activity as MainActivity,
+                object: OnSuccessListener<Location> {
+                    override fun onSuccess(p0: Location?) { // 사용자 위치정보에 대한 값
+                        p0?.let{
+                            val latitude = p0.latitude
+                            val longitude = p0.longitude
+                            Log.d("mobileApp", "lat: $latitude, lng: $longitude")
+                            // 사용자 위치를 카메라로
+                            moveMap(latitude, longitude)
+                        }
+                    }
+                }
+            )
+            apiClient.disconnect()
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
+    }
+
+
 }
