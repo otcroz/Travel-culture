abae31cc75bf4c5c7b546bbe18edc3dea138885d
yxxyn20 <dbtndus0527@gmail.com>
2022-06-17 17:10:42 +0900

    #3 회원가입할 때 사용자에게 정보 받기
    - 닉네임, 이메일, 카테고리, 거주지, UID
    - 사용자에게 input 받은 후 파이어스토어에 반영하기

--- TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/MyApplication.kt
+++ TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/MyApplication.kt
@@ -3,6 +3,8 @@ package com.example.travelcultureapplicaiton
 import androidx.multidex.MultiDexApplication
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
--- TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/RegisterActivity.kt
+++ TravelCultureApplication/app/src/main/java/com/example/travelcultureapplicaiton/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.example.travelcultureapplicaiton
 
+import android.app.AlertDialog
 import android.app.Application
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -9,17 +11,25 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.example.travelcultureapplicaiton.MyApplication.Companion.auth
 import com.example.travelcultureapplicaiton.databinding.ActivityRegisterBinding
 import java.util.*
+import kotlin.collections.ArrayList
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding : ActivityRegisterBinding
-
+    lateinit var userCheckcategory: ArrayList<String>
+    
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var checkboxItems = arrayListOf<String>()
+        userCheckcategory = arrayListOf<String>()
 
         val nextButton = binding.nextSignIn
         val nextSuccess = binding.nextSuccess
@@ -30,8 +40,59 @@ class RegisterActivity : AppCompatActivity() {
         val inputNewpassword = binding.inputNewpassword
         val checkPassword = binding.checkPassword
 
+        // residence Spinner 연결
+        val sAdapter = ArrayAdapter.createFromResource(this, R.array.set_location, android.R.layout.simple_spinner_dropdown_item)
+        binding.residence.adapter = sAdapter;
+
         // 로그인 인텐트
         val intent = Intent(this, LoginActivity::class.java)
+
+        // 회원가입 > 관심 분야 클릭했을 때
+        val dialogCategory: Array<String> = resources.getStringArray(R.array.set_category)
+        var displaySelected = ""
+
+        binding.favoriteCategory.setOnClickListener{
+            AlertDialog.Builder(this).run {
+                setTitle("멀티 아이템 목록 선택")
+                setIcon(android.R.drawable.ic_dialog_info)
+
+                setMultiChoiceItems(dialogCategory, booleanArrayOf(false, false, false, false),
+                    object : DialogInterface.OnMultiChoiceClickListener {
+                        override fun onClick(p0: DialogInterface?, p1: Int, p2: Boolean) {
+                            if(p2){
+                                checkboxItems.add(dialogCategory[p1])
+                            }else{
+                                checkboxItems.remove(dialogCategory[p1])
+                            }
+                        }
+                    }
+                )
+                    // 확인 버튼을 눌렀을 때
+                        setPositiveButton("확인", object : DialogInterface.OnClickListener {
+                            override fun onClick(p0: DialogInterface?, p1: Int) {
+                                displaySelected = ""
+                                userCheckcategory.clear()
+                                for (i in checkboxItems){
+                                    Log.d("appTest", "checkboxItems $i")
+                                }
+                                if (checkboxItems != null) {
+                                    for(check in checkboxItems){
+                                        displaySelected += "$check "
+                                        userCheckcategory.add(check)
+
+                                    }
+                                    Log.d("appTest", "displaySelected $displaySelected")
+                                    binding.favoriteCategory.hint = displaySelected
+                                }
+                            }
+                        }
+                        )
+                checkboxItems.clear()
+                show()
+            }.setCanceledOnTouchOutside(true)
+        }
+
+
 
         //모든 EditText 값 > 0 일때 다음 화면으로 넘어가도록 한다.
         var textWatcher = object : TextWatcher {
@@ -42,7 +103,8 @@ class RegisterActivity : AppCompatActivity() {
                 if (newEmail.length() > 0 && inputNewpassword.length() > 0 && checkPassword.length() > 0){
                     nextButton.isEnabled = true
                     nextButton.setBackgroundColor(Color.parseColor("#B4D480"));
-                    if(residence.length() > 0 && favoriteCategory.length() > 0 && inputName.length() > 0){
+
+                    if(inputName.length() > 0){
                         nextSuccess.isEnabled = true
                         nextSuccess.setBackgroundColor(Color.parseColor("#B4D480"));
                     }
@@ -61,33 +123,36 @@ class RegisterActivity : AppCompatActivity() {
         newEmail.addTextChangedListener(textWatcher)
         inputNewpassword.addTextChangedListener(textWatcher)
         checkPassword.addTextChangedListener(textWatcher)
-        residence.addTextChangedListener(textWatcher)
-        favoriteCategory.addTextChangedListener(textWatcher)
         inputName.addTextChangedListener(textWatcher)
 
         // 다음으로 버튼 클릭
         nextButton.setOnClickListener {
-            nextButton.setBackgroundColor(Color.parseColor("#d3d3d3"))
-            nextButton.text = "입력완료~!"
-            // 버튼 색깔 설정하기
-            inputName.visibility = View.VISIBLE
-            residence.visibility = View.VISIBLE
-            favoriteCategory.visibility = View.VISIBLE
-            nextSuccess.visibility = View.VISIBLE
+            Log.d("appTest", "${inputNewpassword.text}  ${checkPassword.text}")
+            if(inputNewpassword.text.toString() != checkPassword.text.toString()){
+                Toast.makeText(baseContext, "비밀번호를 다시 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else{
+                nextButton.setBackgroundColor(Color.parseColor("#d3d3d3"))
+                nextButton.text = "입력완료~!"
+                // 버튼 색깔 설정하기
+                inputName.visibility = View.VISIBLE
+                residence.visibility = View.VISIBLE
+                favoriteCategory.visibility = View.VISIBLE
+                nextSuccess.visibility = View.VISIBLE
 
-            // 이전에 입력했던 창들은 보이지 않게
-            newEmail.visibility = View.GONE
-            inputNewpassword.visibility = View.GONE
-            checkPassword.visibility = View.GONE
-            nextButton.visibility = View.GONE
-
-
+                // 이전에 입력했던 창들은 보이지 않게
+                newEmail.visibility = View.GONE
+                inputNewpassword.visibility = View.GONE
+                checkPassword.visibility = View.GONE
+                nextButton.visibility = View.GONE
+            }
         }
         // 회원가입 완료
         nextSuccess.setOnClickListener {
             // db에 회원 정보 옮기는 작업 수행
             val email = binding.newEmail.text.toString()
             val password = binding.inputNewpassword.text.toString()
+            // DB에서 해당 이메일로 회원가입한 정보가 있는지 확인하기
+
             MyApplication.auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
 
@@ -119,20 +184,21 @@ class RegisterActivity : AppCompatActivity() {
     private fun saveStore(){ // 이메일, 닉네임, 지역, 카테고리
         Log.d("appTest","유저 테이블 생성하기")
         val data = mapOf(
+            "uid" to auth?.currentUser?.uid.toString(),
             "email" to binding.newEmail.text.toString(), // read, write 권한을 가지도록 하기위해 설정
             "nickname" to binding.inputName.text.toString(),
-            "residence" to binding.residence.text.toString(),
-
-            //"category" to 여러 개의 값을 받는다.
+            "residence" to binding.residence.selectedItem.toString(),
+            "category" to userCheckcategory,
+            "imageFile" to null
         )
 
-        MyApplication.db.collection("user") // 이미지, 글에 대한 정보가 이 테이블에 저장
-            .add(data) // 데이터 업로드
+        MyApplication.db.collection("user").document(auth?.currentUser?.uid.toString()) // 이미지, 글에 대한 정보가 이 테이블에 저장
+            .set(data) // 데이터 업로드
             .addOnSuccessListener{
-                Log.d("mobileApp", "data save success")
+                Log.d("appTest", "data save success")
             }
             .addOnFailureListener{
-                Log.d("mobileApp", "data save error")
+                Log.d("appTest", "data save error")
             }
         binding.newEmail.text.clear()
         binding.inputNewpassword.text.clear()
